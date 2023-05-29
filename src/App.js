9690f966100f59b395f27055cc0e049585c07a77
myself <tkankhediao6@gmail.com>
2023-05-29 21:11:51 +0530

    class based news app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-import LoadingBar from 'react-top-loading-bar'
+import LoadingBar from 'react-top-loading-bar';
 
 import {
   BrowserRouter,
